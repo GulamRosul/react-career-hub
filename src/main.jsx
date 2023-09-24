4393c93caeb037b2d1e7dc8fca7c7bc5c400111b
rosol111 <gulamrosul037@gmail.com>
2023-09-25 03:49:07 +0600

    Added by Load job details page and display

--- src/main.jsx
+++ src/main.jsx
@@ -12,6 +12,7 @@ import Home from './Components/Home/Home';
 import Root from './Components/Root/Root';
 import AppliedJobs from './Components/AppliedJobs/AppliedJobs';
 import ErrorPage from './Components/ErrorPage/ErrorPage';
+import JobDetails from './Components/JobDetails/JobDetails';
 
 
 const router = createBrowserRouter([
@@ -27,6 +28,11 @@ const router = createBrowserRouter([
       {
         path: '/applied',
         element: <AppliedJobs></AppliedJobs>
+      },
+      {
+        path: '/job/:id',
+        element: <JobDetails></JobDetails>,
+        loader: () => fetch('../jobs.json')  //do not load all data. load only what you need
       }
     ]
   },
